23f6554908537a883ec3b674a1b718a26dee097a
Piske <marcelopiske@ideyas.com>
2022-03-20 21:17:27 -0300

    subindo settings

--- storagemanage/storagemanage/settings.py
+++ storagemanage/storagemanage/settings.py
@@ -130,8 +130,8 @@ MEDIA_URL = '/media/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # conexão com S3 azure
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+""" DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 AWS_S3_ACCESS_KEY_ID = "AKIATPFAVZUOOJNH2IF5"
 AWS_S3_SECRET_ACCESS_KEY = "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"
 AWS_STORAGE_BUCKET_NAME = "djangofreeconnect"
-AWS_QUERYSTRING_AUTH = False
+AWS_QUERYSTRING_AUTH = False """
